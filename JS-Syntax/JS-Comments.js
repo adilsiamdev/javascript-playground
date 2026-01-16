0504bb255747178a05dde103f017c1fc6d25a4a4
adilsiamdev <adilsiam.dev@gmail.com>
2026-01-16 17:03:39 +0600

    Adil:Added js syntax

---
+++ JS-Syntax/JS-Comments.js
@@ -0,0 +1,30 @@
+//Single Line Comments
+//Single line comments start with //.
+
+//Any text between // and the end of the line will be ignored by JavaScript (will not be executed).
+
+//This example uses a single-line comment before each code line:
+
+// Change heading:
+document.getElementById("myH").innerHTML = "My First Page";
+
+// Change paragraph:
+document.getElementById("myP").innerHTML = "My first paragraph.";
+
+
+//Multi-line Comments
+//Multi-line comments start with /* and end with */.
+
+//Any text between /* and */ will be ignored by JavaScript.
+
+//This example uses a multi-line comment (a comment block) to explain the code:
+
+/*
+The code below will change
+the heading with id = "myH"
+and the paragraph with id = "myP"
+in my web page:
+*/
+document.getElementById("myH").innerHTML = "My First Page";
+document.getElementById("myP").innerHTML = "My first paragraph.";
+
