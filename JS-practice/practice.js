adc3d8bfa122e4904f501b4fc5219b0d06df75bb
adilsiamdev <adilsiam.dev@gmail.com>
2026-01-28 13:32:01 +0600

    WIP

---
+++ JS-practice/practice.js
@@ -0,0 +1,10 @@
+function getFullName(fName, mName, lName) {
+    return fName + mName + " " + lName;
+}
+console.log(getFullName("Md.", "Adil", "Siam")); 
+
+
+function chekUpDoctor(name, age) {
+    return "Patient " + name + " is " + age + " years old.";
+}
+console.log(chekUpDoctor("Adil", 24));
