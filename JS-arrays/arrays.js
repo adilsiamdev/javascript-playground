9800d4bc08840b3c57accc0527086aa8118e0d45
adilsiamdev <adilsiam.dev@gmail.com>
2026-02-17 19:35:15 +0600

    wip

---
+++ JS-arrays/arrays.js
@@ -0,0 +1,12 @@
+//Array
+const person = ["Age=25", "Name=Adil", "City=Gaibandha", "country=Bangladesh"]
+console.log(person);
+//Object
+const person1 = {
+    Age : 22,
+    Name : "Adil",
+    City : "Gaibandha",
+    country : "bangladeshh",
+}
+console.log(person1);
+console.log(person1.Name);
