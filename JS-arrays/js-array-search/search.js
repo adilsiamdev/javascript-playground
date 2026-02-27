2cfd0e68b69e13245e7c066094524e8460b15fbd
adilsiamdev <adilsiam.dev@gmail.com>
2026-02-28 01:23:30 +0600

    JS array indexof topic

---
+++ JS-arrays/js-array-search/search.js
@@ -0,0 +1,7 @@
+//indexof
+//The indexOf() method searches an array for an element value and returns its position.
+const fruits = ["Apple", "Orange", "Apple", "Mango"]; //index jehetu 0 theke count hoy
+let position = fruits.indexOf("Mango");
+console.log(fruits);
+console.log("Mango is found in position" + "",position);
+
