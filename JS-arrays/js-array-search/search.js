a443538a3592b705364fb8cb539ad4aaf6736989
adilsiamdev <adilsiam.dev@gmail.com>
2026-02-28 01:32:55 +0600

    JS array lastindexoff topic

--- JS-arrays/js-array-search/search.js
+++ JS-arrays/js-array-search/search.js
@@ -1,7 +1,11 @@
 //indexof
 //The indexOf() method searches an array for an element value and returns its position.
-const fruits = ["Apple", "Orange", "Apple", "Mango"]; //index jehetu 0 theke count hoy
-let position = fruits.indexOf("Mango");
+const fruits = ["Apple", "Orange", "Apple", "Mango"]; //index jehetu 0 theke count hoy but position amra display korabo so 1 thekei count korbe normal human
+let position = fruits.indexOf("Mango") +1;
 console.log(fruits);
 console.log("Mango is found in position" + "",position);
+
+//Array.lastIndexOf() is the same as Array.indexOf(), but returns the position of the last occurrence of the specified element.
+let position1 = fruits.lastIndexOf("Apple") +1 //+1 deoar karon position index er moto 0 theke suru hoy na
+console.log("Apple is found in position" + "", position)
 
