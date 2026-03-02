fb0d47ad4cb0a9a24bb141fde1091cda8a5cc58a
adilsiamdev <adilsiam.dev@gmail.com>
2026-03-03 04:28:36 +0600

    sort reverse that show descending order

--- JS-arrays/js-array-sort/sort.js
+++ JS-arrays/js-array-sort/sort.js
@@ -3,4 +3,9 @@ const fruits = ["Banana", "Orange", "Apple", "Mango"];
 fruits.sort();
 console.log(fruits);
 
+////The sort() method can be used to sort an array in descending order:
+const fruits1 = ["Banana", "Orange", "Apple", "Mango"];
+fruits1.sort();
+fruits1.reverse();
+console.log(fruits1);
 
