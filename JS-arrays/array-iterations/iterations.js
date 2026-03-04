c4cafe1c69213bf44f98050f064e20a72fbf07a3
adilsiamdev <adilsiam.dev@gmail.com>
2026-03-05 05:49:20 +0600

    js map method

--- JS-arrays/array-iterations/iterations.js
+++ JS-arrays/array-iterations/iterations.js
@@ -1,7 +1,26 @@
 //The forEach() method calls a function (a callback function) once for each array element.
 const numbers = [45, 4, 9, 16, 25];
-numbers.forEach(myFunction);
+numbers.forEach(myFunction1);
 
-function myFunction(value) {
+function myFunction1(value) {
   console.log(value);
 }
+
+//The map() method creates a new array by performing a function on each array element.
+
+//The map() method does not execute the function for array elements without values.
+
+//The map() method does not change the original array.
+const numbers1 = [45, 4, 9, 16, 25];
+const numbers2 = numbers1.map(myFunction);
+
+function myFunction(value) {
+  return value * 2;
+}
+console.log(numbers2);const numbers3 = [45, 4, 9, 16, 25];
+const numbers4 = numbers3.map(myFunction);
+
+function myFunction(value) {
+  return value * 3;
+}
+console.log(numbers4);
