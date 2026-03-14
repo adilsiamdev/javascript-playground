e41ef54798a0ac92ff03d0c959f684ff894eae5f
adilsiamdev <adilsiam.dev@gmail.com>
2026-03-14 14:23:11 +0600

    without break in switch function

--- JS-conditional/js-switch.js
+++ JS-conditional/js-switch.js
@@ -40,3 +40,40 @@ switch (date) {
 }
 
 console.log(day); // Output: (current day of the week)
+
+//note: The getDay() method returns the weekday as a number between 0 and 6.
+//(Sunday=0, Monday=1, Tuesday=2 ..)
+
+//.........................
+//When JavaScript reaches a break keyword, it breaks out of the switch block.
+
+//This will stop the execution inside the switch block.
+//No more statements in the switch block will be executed.
+//It is not necessary to break the last case. The switch ends (breaks) there anyway.
+
+//note: The break keyword is crucial for preventing a "fall-through." Without break, the code will continue to execute the next case blocks (and the default block if present) even if their values do not match the expression.
+
+//Example without break
+let day2;
+let date2 = new Date().getDay();
+    
+switch (date2) {
+    case 0:
+        day2 = "Sunday";
+        case 1:
+        day2 = "Monday";
+        case 2:
+        day2 = "Tuesday";
+        case 3:
+        day2 = "Wednesday";
+        case 4:
+        day2 = "Thursday";
+        case 5:
+        day2 = "Friday";
+        case 6:
+        day2 = "Saturday";
+}
+
+console.log(day2); // Output: (current day of the week)
+
+//.........................
