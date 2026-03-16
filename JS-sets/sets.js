66de082062792497f296147274781e2c40ee393b
adilsiamdev <adilsiam.dev@gmail.com>
2026-03-16 17:14:42 +0600

    js sets intro and sets method Added

---
+++ JS-sets/sets.js
@@ -0,0 +1,11 @@
+//A JavaScript Set is a collection of unique values.
+//Each value can only occur once in a Set.
+//The values can be of any type, primitive values or objects.
+//How to Create a Set
+//You can create a JavaScript Set by:
+//Passing an array to new Set()
+//Create an empty set and use add() to add values
+//The new Set() Method
+//Pass an array to the new Set() constructor:
+const letters = new Set(["a", "b", "c"]);
+console.log("The set has " + letters.size + " values.")
