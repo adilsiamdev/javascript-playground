d8fd6c3222ad34e8f229f018149e2809f95bf048
adilsiamdev <adilsiam.dev@gmail.com>
2026-05-13 15:30:32 +0600

    js Asynchronus

--- sync&async/sync&async.js
+++ sync&async/sync&async.js
@@ -11,3 +11,16 @@ console.log("take order for customer 1");
 processOrder();
 console.log("take order for customer 2");
 processOrder();
+
+//Async.js
+const processOrderAsync = (customer) => {
+  console.log("Processing Order for customer 1");
+  setTimeout(() => {
+    console.log("Cooking Compleated");
+  }, 6000);
+};
+
+console.log("take order for customer 1");
+processOrderAsync();
+console.log("take order for customer 2");
+processOrderAsync();
